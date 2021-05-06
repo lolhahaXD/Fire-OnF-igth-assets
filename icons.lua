946cafb84fbb934d7f79fb1a85af20339c9add8d
lolhahaXD <81463152+lolhahaXD@users.noreply.github.com>
2021-05-06 14:08:14 +0200

    Create icons.lua

---
+++ icons.lua
@@ -0,0 +1,7 @@
+when_bar_80%_show_bad_icons
+when_bar_10%_show_monday_bad.png
+monday_appear_on:all levels
+odler_appear:level 1
+whitedbrod_appear:level 2
+porpla_appear:level 3
+Darklox(boss)_appear:level 4
